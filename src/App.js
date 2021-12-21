dde8db5c9203ca39a836f388ccc2c53c87e790d8
Ольга Турова <you@example.com>
2021-12-22 00:17:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './App.css';
+import { Heart } from './Heart';
+
+class App extends Component {
+
+  render() {
+    return <Heart/>
+  }
+}
+
+export default App;
---
+++ src/Heart.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import emptyheart from "./emptyheart.jpg";
+import fullheart from "./fullheart.jpg"
+
+export class Heart extends Component {
+
+    state = {
+        CTA: "LIKE US!",
+        image: emptyheart
+      }
+
+      heartClicked = () =>{
+          this.setState({
+              CTA: "Thank you! We love you? too!",
+              image: fullheart
+          })
+      }
+
+    render() {
+        return <div>
+            <div  className="container">
+            <h1>{this.state.CTA}</h1>
+            </div>
+            <div  className="container">
+        <img onClick={this.heartClicked} src={this.state.image} width="120px" alt="heart"/>
+        </div>
+      </div>
+    }
+}
